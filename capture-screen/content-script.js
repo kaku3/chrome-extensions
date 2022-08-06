d65c4b7c00b237f6b2e2a0ba4f45d6aba7fae576
kaku3 <kodakaku@gmail.com>
2022-08-06 23:48:22 +0900

    feat: clipboard 処理

--- capture-screen/content-script.js
+++ capture-screen/content-script.js
@@ -7,8 +7,62 @@ chrome.runtime.onMessage.addListener(function(request, sender, response) {
 })
 
 function download(o) {
-  const d = document.createElement('a');
-  d.href = 'data:image/png;base64,' + o.data;
-  d.download = o.fileName;
-  d.click()  
+  const dataUrl = 'data:image/png;base64,' + o.data
+  const e = document.createElement('a')
+  e.href = dataUrl
+  e.download = o.fileName
+  e.click()
+
+  const blob = new Blob([base64ToArrayBuffer(o.data)], { type: 'image/png'})
+  try{
+    navigator.clipboard.write([
+      new ClipboardItem({
+        'image/png': blob
+      })
+    ]);
+  }catch(err){
+    // 2022/08/06
+    // navigator.clipboard.write は、https サイトでしか動作しない。
+    // http サイトの場合は、document.execCommand('copy') でコピーをしようとしたが、
+    // document.execCommand('copy') は、非推奨 -> 廃止となった模様
+    // copyToClipboard()
+  }
+}
+
+/**
+ * 2022/08/06 : document.execCommand('copy') is not working.
+ * 記録として残しておく
+ */
+function copyToClipboard() {
+  const img = document.createElement('img')
+  img.onload = function() {
+    const c = document.createElement('div')
+    c.contentEditable = 'true'
+    c.appendChild(img)
+    document.body.appendChild(c)
+    selectElement(c)
+    document.execCommand('copy')
+    window.getSelection().removeAllRanges()
+    document.body.removeChild(c)
+  }
+  img.src = dataUrl
+}
+
+function selectElement(element) {
+  const range = document.createRange()
+  range.selectNodeContents(element)
+
+  const selection = window.getSelection()
+  selection.removeAllRanges()
+  selection.addRange(range)
+}
+
+function base64ToArrayBuffer(base64) {
+  const decoded = window.atob(base64)
+  const len = decoded.length
+  const bytes = new Uint8Array(len)
+  for (let i = 0; i < len; i++) {
+    bytes[i] = decoded.charCodeAt(i)
+  }
+  return bytes.buffer
 }
